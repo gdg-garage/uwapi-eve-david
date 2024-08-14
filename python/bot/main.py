1bdd52b9e1ca2eed02d1ec5973181dd9cd3ec5f2
Eva Mayerova <evca.mayerova@gmail.com>
2024-08-14 20:41:22 +0200

    build first drill

--- python/bot/main.py
+++ python/bot/main.py
@@ -1,19 +1,72 @@
 import os
 import random
+from collections import defaultdict
+
 import uw
+from uw import Prototype
 
 
 class Bot:
     def __init__(self):
         self.game = uw.Game()
         self.step = 0
+        self.main_building = None
+        self.resources_map = None
+        self.prototypes = None
+        self.constructions = None
 
         # register update callback
         self.game.add_update_callback(self.update_callback_closure())
 
+    def find_main_base(self):
+        if self.main_building:
+            return
+        for e in self.game.world.entities().values():
+            if not (e.own() and hasattr(e, "Unit")):
+                continue
+            unit = self.game.prototypes.unit(e.Proto.proto)
+            if not unit:
+                continue
+            if unit.get("name", "") == "nucleus":
+                self.main_building = e
+
+    def init_prototypes(self):
+        if self.prototypes:
+            return
+        self.prototypes = []
+        for p in self.game.prototypes.all():
+            self.prototypes.append({
+                "id": p,
+                "name": self.game.prototypes.name(p),
+                "type": self.game.prototypes.type(p),
+            })
+        self.constructions = list(filter(lambda x: x["type"] == Prototype.Construction, self.prototypes))
+        print("=======")
+        print(self.constructions)
+
+    def get_closest_ores(self):
+        self.resources_map = defaultdict(list)
+        for e in self.game.world.entities().values():
+            if not (hasattr(e, "Unit")) and not e.own():
+                continue
+            unit = self.game.prototypes.unit(e.Proto.proto)
+            if not unit:
+                continue
+            if "deposit" not in unit.get("name", ""):
+                continue
+            name = unit.get("name", "").replace(" deposit", "")
+            self.resources_map[name].append(e)
+        if not self.main_building:
+            return
+        for r in self.resources_map:
+            sorted(self.resources_map[r], key=lambda x: self.game.map.distance_estimate(
+                        self.main_building.Position.position, x.Position.position
+                    ))
+
     def start(self):
         self.game.log_info("starting")
         self.game.set_player_name("eve-david")
+
         if not self.game.try_reconnect():
             self.game.set_start_gui(True)
             lobby = os.environ.get("UNNATURAL_CONNECT_LOBBY", "")
@@ -24,7 +77,7 @@ class Bot:
             elif addr != "" and port != "":
                 self.game.connect_direct(addr, port)
             else:
-                self.game.connect_new_server(extra_params="-m special/combat-test.uw")
+                self.game.connect_new_server(extra_params="-m planets/triangularprism.uw")
         self.game.log_info("done")
 
     def attack_nearest_enemies(self):
@@ -72,14 +125,39 @@ class Bot:
             if len(recipes) > 0:
                 self.game.commands.command_set_recipe(e.Id, random.choice(recipes))
 
+    def build_drill(self, deposit: uw.world.Entity):
+        print("hello")
+        for c in self.constructions:
+            print(c["name"])
+            if c["name"] == "drill":
+                self.game.commands.command_place_construction(c["id"], deposit.Position.position)
+
+    def maybe_build_iron_drill(self):
+        if self.resources_map is None:
+            return
+        closest_metal_deposits : list = self.resources_map.get("metal", [])
+        # check if enough reinforced concrete
+        if len(closest_metal_deposits) > 0:
+            self.build_drill(closest_metal_deposits[0])
+        pass
+
     def update_callback_closure(self):
         def update_callback(stepping):
             if not stepping:
                 return
             self.step += 1  # save some cpu cycles by splitting work over multiple steps
 
+            self.init_prototypes()
+
+            if self.resources_map is None:
+                self.get_closest_ores()
+                print("====== closest ores ======")
+                print(self.resources_map)
+
             if self.step % 10 == 1:
                 self.attack_nearest_enemies()
+
+            self.maybe_build_iron_drill()
 
             if self.step % 10 == 5:
                 self.assign_random_recipes()
